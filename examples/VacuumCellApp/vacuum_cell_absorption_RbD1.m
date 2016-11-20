6db69b2a8759bb57ba750e1838558eeb0f73c9a9
nzhao <nzhao@csrc.ac.cn>
2016-11-20 19:57:11 +0800

    improve.

--- examples/VacuumCellApp/vacuum_cell_absorption_RbD1.m
+++ examples/VacuumCellApp/vacuum_cell_absorption_RbD1.m
@@ -1,4 +1,4 @@
-function vacuum_cell_absorption_RbD1(output_file, varargin)
+function [freqList, res_absorption] = vacuum_cell_absorption_RbD1(output_file, varargin)
 %VACUUM_CELL_ABSORPTION Summary of this function goes here
 %   Detailed explanation goes here
     import Condition.Coil
@@ -8,8 +8,11 @@ function vacuum_cell_absorption_RbD1(output_file, varargin)
     import CellSystem.VacuumCell
 
 %% parse args
-    start_at = datetime('now');
     code_version = CodeVersion();
+    fprintf('Using code with commit id: %s\n', code_version);
+    start_at = datetime('now'); disp(start_at);
+
+%% parameters
     default_parameters = containers.Map();
     default_parameters('Bx') = 0.00001;       % [Tesla]
     default_parameters('By') = 0.0;           % [Tesla]
@@ -23,26 +26,21 @@ function vacuum_cell_absorption_RbD1(output_file, varargin)
     
     parameters = parse_paremeters(default_parameters, varargin);
     
-    disp(start_at);
-    fprintf('Using code with commit id: %s\n', code_version);
     disp(keys(parameters));
     disp(values(parameters));
     
 %% ingredients - system
-    coil = { ... 
-        Condition.Coil('coilx', parameters('Bx') ), ...
-        Condition.Coil('coily', parameters('By') ), ...
-        Condition.Coil('coilz', parameters('Bz') ) };
+    coil = { Condition.Coil('coilx', parameters('Bx') ), ...
+             Condition.Coil('coily', parameters('By') ), ...
+             Condition.Coil('coilz', parameters('Bz') ) };
 
     rb85=AlkaliMetal('85Rb', coil);
     rb87=AlkaliMetal('87Rb', coil);
 
     gases={  Gas(rb85, 'vapor', 273.15+parameters('Temperature'), Atom.Transition.D1), ...
-             Gas(rb87, 'vapor', 273.15+parameters('Temperature'), Atom.Transition.D1) ...
-             };
+             Gas(rb87, 'vapor', 273.15+parameters('Temperature'), Atom.Transition.D1)};
 
-    pumpBeam=AlkaliLaserBeam(parameters('Power'), ... 
-                             rb87, Atom.Transition.D1, 0.0,... 
+    pumpBeam=AlkaliLaserBeam(parameters('Power'), rb87, Atom.Transition.D1, 0.0,... 
                              [0 0 1], parameters('Polarization'), parameters('BeamRadius')); 
     
     sys=VacuumCell(gases, pumpBeam);
@@ -51,15 +49,13 @@ function vacuum_cell_absorption_RbD1(output_file, varargin)
     freqList = parameters('Frequency'); t_pump = parameters('PumpTime');
     res_absorption = sys.total_absorption_cross_section(freqList, t_pump);
 
-    finish_at = datetime('now');
-    disp(finish_at);
+    
 %% export
-    save(output_file, 'start_at', 'code_version', 'parameters', ...
+    finish_at = datetime('now'); disp(finish_at);
+
+    save(output_file, 'start_at', 'finish_at', 'code_version', 'parameters', ...
                       'coil', 'rb85', 'rb87', 'gases', 'pumpBeam', 'sys', ...
                       'freqList', 'res_absorption');
-    figure; 
-    plot(freqList, res_absorption, freqList, sum(res_absorption,1), 'kd-');
-    title(['Power = ',  num2str(parameters('Power')) 'W']);
 
 end
 
