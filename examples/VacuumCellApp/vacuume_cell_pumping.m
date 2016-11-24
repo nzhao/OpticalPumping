c7bc98516e90038e791b7e5d9104bc87740d547f
nzhao <nzhao@csrc.ac.cn>
2016-11-24 15:28:59 +0800

    effective Hamiltonian

--- examples/VacuumCellApp/vacuume_cell_pumping.m
+++ examples/VacuumCellApp/vacuume_cell_pumping.m
@@ -20,12 +20,12 @@ gases={  Gas(rb85, 'vapor', temperature, Atom.Transition.D1), ...
          Gas(rb87, 'vapor', temperature, Atom.Transition.D1) ...
          };
 
-pumpBeam=AlkaliLaserBeam(5e-6, ...                     % power in [W]
+pumpBeam=AlkaliLaserBeam(0.25e-6, ...                     % power in [W]
                          rb87, Atom.Transition.D1, -3064,...%-2.25e3, ... % ref Atom 
                          [0 0 1], [1, 0], 2e-3);       % direction, pol, spot size
                      
 
 %%
 t_pump = 10.0;
-sys=VacuumCell(gases, pumpBeam.set_detuning(-2500));
+sys=VacuumCell(gases, pumpBeam.set_detuning(-2.25e3));
 vData=sys.velocity_resolved_pumping(3, t_pump, 'diagnose');
---
+++ phys/+Interaction/@VaporBeamInteraction/matrix_effective_Hamiltonian.m
@@ -0,0 +1,33 @@
+function matrix_effective_Hamiltonian( obj )
+%MATRIX_EFFECTIVE_HAMILTONIAN Summary of this function goes here
+%   Detailed explanation goes here
+
+    Dk = obj.beam.refTransition; % D1 or D2
+    dimG = obj.vapor.atom.dim(1);
+    dimE = obj.vapor.atom.dim(1+Dk);
+    
+    denom = Interaction.DenominatorMat(obj.vapor, obj.beam, obj.parameter.velocity);
+    tV =  Interaction.AtomPhotonInteraction(obj.vapor, obj.beam);
+    tW = tV.*denom;
+
+    Hg = -tV'*tW;  He = tV*tW';
+    shift_g = 0.5*(Hg+Hg'); gamma_g = 1i*(Hg-Hg');
+    shift_e = 0.5*(He+He'); gamma_e = 1i*(He-He');
+    pump_rate_g = trace(gamma_g)/dimG;
+    pump_rate_e = trace(gamma_e)/dimE;
+
+    mat_ge = zeros(dimG, dimE); mat_eg=zeros(dimE, dimG);
+    gamma_col = [gamma_e(:); mat_eg(:); mat_ge(:); gamma_g(:)];
+
+    obj.matrix.eff_Hg = Hg;
+    obj.matrix.eff_He = He;
+    obj.matrix.shift_e = shift_e;
+    obj.matrix.shift_g = shift_g;
+    obj.matrix.gamma_e = gamma_e;
+    obj.matrix.gamma_g = gamma_g;
+    obj.matrix.gamma_col = gamma_col;
+    obj.parameter.pump_rate_g = pump_rate_g;
+    obj.parameter.pump_rate_e = pump_rate_e;
+
+end
+
