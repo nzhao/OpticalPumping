883391d5a1fc21f4f54ed03d48f1670c97db52d3
nzhao <nzhao@csrc.ac.cn>
2017-02-08 21:07:50 +0800

    spin Bloch model
    complete the live-script, and revise the simulink model.

--- const/PROJ_PATH.m
+++ const/PROJ_PATH.m
@@ -2,6 +2,7 @@ function val = PROJ_PATH( )
 %PROJ_PATH Summary of this function goes here
 %   Detailed explanation goes here
 val = strrep(mfilename('fullpath'), '/const/PROJ_PATH','');
+val = strrep(val, '\const\PROJ_PATH',''); % for windows
 %val = '/Users/nzhao/code/lib/active/OpticalPumping';
 
 end
